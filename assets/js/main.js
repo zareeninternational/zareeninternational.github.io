cd9bdaa7fbc0e25db79c731e25a76b1627052dc5
zareeninternational <terahotel2@gmail.com>
2025-12-05 14:24:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+/* MOBILE MENU TOGGLE */
 function toggleMenu() {
     document.getElementById("mobileMenu").classList.toggle("active");
 }
+
+/* CLOSE MENU WHEN CLICKING A LINK */
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("mobileMenu").classList.remove("active");
+    });
+});
